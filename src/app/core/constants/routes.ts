6a3c8e6b20625819ac6d1b7250ff29f213e03ced
Sergi Jimenez <acasualcolumbus@gmail.com>
2024-01-11 19:23:46 +0100

    Closes Created connection between pages #7

---
+++ src/app/core/constants/routes.ts
@@ -0,0 +1,6 @@
+export class RoutesConst {
+  public static readonly HOMEPATH = "home";
+  public static readonly ASSIGNPATH = "assign";
+  public static readonly HOME = `/${this.HOMEPATH}`;
+  public static readonly ASSIGN = `/${this.ASSIGNPATH}`;
+}
