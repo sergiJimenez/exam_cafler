2da0ec3964f9014fe7050e714a9b8e1b2aa8268b
Sergi Jimenez <acasualcolumbus@gmail.com>
2024-01-12 22:26:53 +0100

    Checkpoint

--- src/app/features/pages/assign_page/views/assign_page.component.ts
+++ src/app/features/pages/assign_page/views/assign_page.component.ts
@@ -5,6 +5,11 @@ import { IOptimizedRoutes } from "src/app/shared/interfaces/optimized-routes.int
 import { LocalStorageService } from "src/app/core/services/localStorage.service";
 import { IRiders } from "src/app/shared/interfaces/riders.interface";
 import { RidersService } from "src/app/features/services/riders.service";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: "app-assign-page",
@@ -15,6 +20,8 @@ export class AssignPageComponent implements OnInit {
   public optimizedRoutes$!: Observable<IOptimizedRoutes[]>;
   public optimizedRoutesDetails: IOptimizedRoutes | undefined;
   public riders$!: Observable<IRiders[]>;
+  todo = ["Get to work", "Pick up groceries", "Go home", "Fall asleep"];
+  done = ["Get up", "Brush teeth", "Take a shower", "Check e-mail", "Walk dog"];
 
   constructor(
     public ls: LocalStorageService,
@@ -25,6 +32,23 @@ export class AssignPageComponent implements OnInit {
   public ngOnInit(): void {
     this.loadOptimizedRoutes();
     this.loadRiders();
+  }
+
+  public drop(event: CdkDragDrop<string[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 
   private loadOptimizedRoutes(): void {
