7d2412a0418780cbc34c5dc47b3657b4f6f0a329
Sergi Jimenez <acasualcolumbus@gmail.com>
2024-01-13 02:14:30 +0100

    Checkpoint

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, computed, signal } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
 import { RoutesConst } from "src/app/core/constants/routes";
 
@@ -8,25 +8,35 @@ import { RoutesConst } from "src/app/core/constants/routes";
   styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
-  public pages: { name: string; route: string }[] = [
-    { name: "navItem1", route: RoutesConst.HOME }
-  ];
+  public isDarkThemeOn = signal(false);
+  public icon = computed(() =>
+    this.isDarkThemeOn() ? "light_mode" : "dark_mode"
+  );
 
   constructor(public translate: TranslateService) {
     translate.addLangs(["es", "cat", "eusk", "en", "fr", "it"]);
 
-    if (localStorage.getItem("LocalLanguage")) {
-      translate.setDefaultLang(localStorage.getItem("LocalLanguage") || "[]");
-      translate.use(localStorage.getItem("LocalLanguage") || "[]");
+    if (localStorage.getItem("locale")) {
+      translate.setDefaultLang(localStorage.getItem("locale") || "[]");
+      translate.use(localStorage.getItem("locale") || "[]");
     } else {
       const browserLang = translate.getBrowserLang();
       translate.setDefaultLang(browserLang || "[]");
       translate.use(browserLang?.match(/es|en/) ? browserLang : "es");
-      localStorage.setItem("LocalLanguage", browserLang || "[]");
+      localStorage.setItem("locale", browserLang || "[]");
     }
   }
 
-  public switchLanguage(lang: string) {
+  public pages: { name: string; route: string }[] = [
+    { name: "navItem1", route: RoutesConst.HOME },
+  ];
+
+  public switchLanguage(lang: string): void {
     this.translate.use(lang);
+  }
+
+  public toggleDarkTheme(): void {
+    document.body.classList.toggle("dark-theme");
+    this.isDarkThemeOn.update((isDarkThemeOn) => !isDarkThemeOn);
   }
 }
