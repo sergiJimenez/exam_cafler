3818fac06412fa5eadec8a6a9f6d2614294b25ba
Sergi Jimenez <acasualcolumbus@gmail.com>
2024-01-13 20:46:05 +0100

    Checkpoint

--- src/app/features/pages/home_page/home_page.module.ts
+++ src/app/features/pages/home_page/home_page.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from "@angular/common";
 import { HomePageRoutingModule } from "./home_page-routing.module";
 import { HomePageComponent } from "./views/home_page.component";
 import { SharedModule } from "src/app/shared/shared.module";
-
+import { MatTableModule } from "@angular/material/table";
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, HomePageRoutingModule, SharedModule],
+  imports: [CommonModule, HomePageRoutingModule, SharedModule, MatTableModule],
 })
 export class HomePageModule {}
