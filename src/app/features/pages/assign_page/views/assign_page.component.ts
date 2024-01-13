8333904fc821c7e8ac099febe7d2a51d01c1a061
Sergi Jimenez <acasualcolumbus@gmail.com>
2024-01-13 10:46:22 +0100

    Checkpoint

--- src/app/features/pages/assign_page/views/assign_page.component.ts
+++ src/app/features/pages/assign_page/views/assign_page.component.ts
@@ -5,11 +5,11 @@ import { IOptimizedRoutes } from "src/app/shared/interfaces/optimized-routes.int
 import { LocalStorageService } from "src/app/core/services/localStorage.service";
 import { IRiders } from "src/app/shared/interfaces/riders.interface";
 import { RidersService } from "src/app/features/services/riders.service";
-import {
-  CdkDragDrop,
-  moveItemInArray,
-  transferArrayItem,
-} from "@angular/cdk/drag-drop";
+// import {
+//   CdkDragDrop,
+//   moveItemInArray,
+//   transferArrayItem,
+// } from "@angular/cdk/drag-drop";
 
 @Component({
   selector: "app-assign-page",
@@ -20,8 +20,8 @@ export class AssignPageComponent implements OnInit {
   public optimizedRoutes$!: Observable<IOptimizedRoutes[]>;
   public optimizedRoutesDetails: IOptimizedRoutes | undefined;
   public riders$!: Observable<IRiders[]>;
-  todo = ["Get to work", "Pick up groceries", "Go home", "Fall asleep"];
-  done = ["Get up", "Brush teeth", "Take a shower", "Check e-mail", "Walk dog"];
+  // todo = ["Get to work", "Pick up groceries", "Go home", "Fall asleep"];
+  // done = ["Get up", "Brush teeth", "Take a shower", "Check e-mail", "Walk dog"];
 
   constructor(
     public ls: LocalStorageService,
@@ -34,22 +34,22 @@ export class AssignPageComponent implements OnInit {
     this.loadRiders();
   }
 
-  public drop(event: CdkDragDrop<string[]>): void {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    }
-  }
+  // public drop(event: CdkDragDrop<string[]>): void {
+  //   if (event.previousContainer === event.container) {
+  //     moveItemInArray(
+  //       event.container.data,
+  //       event.previousIndex,
+  //       event.currentIndex
+  //     );
+  //   } else {
+  //     transferArrayItem(
+  //       event.previousContainer.data,
+  //       event.container.data,
+  //       event.previousIndex,
+  //       event.currentIndex
+  //     );
+  //   }
+  // }
 
   private loadOptimizedRoutes(): void {
     this.optimizedRoutes$ = this.optimizedRoutes.getOptimizedRoutes();
