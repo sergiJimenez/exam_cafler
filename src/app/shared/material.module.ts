156053b8d6895907400ecfb3a5207b0255826b8c
Sergi Jimenez <acasualcolumbus@gmail.com>
2024-01-14 14:36:54 +0100

    Include matToolTip

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -3,12 +3,15 @@ import { MatDialogModule } from "@angular/material/dialog";
 import { MatSelectModule } from "@angular/material/select";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatButtonModule } from "@angular/material/button";
+import { MatTooltipModule } from "@angular/material/tooltip";
+
 const MATERIAL_MODULES = [
   MatDialogModule,
   MatSelectModule,
   MatFormFieldModule,
   MatSelectModule,
   MatButtonModule,
+  MatTooltipModule,
 ];
 @NgModule({
   imports: [MATERIAL_MODULES],
